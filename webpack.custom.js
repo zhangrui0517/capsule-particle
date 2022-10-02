8c939a5d17ddaf55b61788ed079932734c5b48bd
zhangrui <393394595@qq.com>
2022-10-02 15:29:59 +0800

    fix: 调整代码格式化规范；增加自定义构建配置；

--- webpack.custom.js
+++ webpack.custom.js
@@ -1,12 +1,26 @@
 const config = {
   /** 要打包的文件根目录 */
-  // root: 'src',
+  root: 'test',
   /** 是否需要动态polyfill注入 */
   // dynamicPolyfill: true
   /** 是否启用babel对react的解析 */
   // react: true,
   /** webpack config 自定义调整，会通过webpack.merge与预设webpack配置合并 */
-  // config: (webpackConfig) => webpackConfig
+  config: webpackConfig => {
+    const devServer = webpackConfig.devServer || {}
+    const devServerClient = devServer.client || {}
+    webpackConfig.devServer = {
+      ...devServer,
+      client: {
+        ...devServerClient,
+        overlay: {
+          errors: true,
+          warnings: false
+        }
+      }
+    }
+    return webpackConfig
+  }
 }
 
 module.exports = config
