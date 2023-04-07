1a2995eb2578c922c3b44fdd990457fc073e994d
zhangrui <393394595@qq.com>
2023-04-07 08:32:21 +0800

    fix: clean webpack config

--- test/index.ts
+++ test/index.ts
@@ -2,12 +2,15 @@ import Particle, { IOption } from '../src'
 import { description } from './data'
 
 function controller(descItem: IOption['description']) {
+  // console.log('descItem: ', descItem)
   descItem
 }
 
+console.time('particleObj')
 const particleObj = new Particle({
   description,
   controller
 })
+console.timeEnd('particleObj')
 
 console.log('particleObj', particleObj)
